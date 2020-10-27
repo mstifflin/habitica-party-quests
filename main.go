73acadefcf1815932ee727de2d70b1b4237c89d1
Tiffany Lin <mstifflin@gmail.com>
2020-10-27 14:49:40 -0700

    Make main run indefinitely

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 const (
@@ -12,73 +13,77 @@ const (
 func main() {
 	apiKey := os.Args[1]
 
-	// Get the user's party ID
-	partyID, err := getPartyID(myUUID)
-	if err != nil {
-		fmt.Println("error getting party id", err.Error())
-		return
-	}
-
-	// Given a partyID, get all the party's members names and UUIDs
-	partyMembers, err := getPartyMembers(myUUID, apiKey, partyID)
-	if err != nil {
-		fmt.Println("error getting party members", err.Error())
-		return
-	}
-
-	// Given a list of party members, get all the quests they
-	// have in their inventory
-	allUserQuests := []map[string]int{}
-	partyMemberToQuestInventoryMap := map[string]map[string]int{}
-	for _, member := range partyMembers {
-		userQuests, err := getUserQuests(member.UserUUID)
+	for true {
+		// Get the user's party ID
+		partyID, err := getPartyID(myUUID)
 		if err != nil {
-			fmt.Println("error getting party members quests", err.Error())
+			fmt.Println("error getting party id", err.Error())
 			return
 		}
 
-		partyMemberToQuestInventoryMap[member.UserName] = userQuests
-		allUserQuests = append(allUserQuests, userQuests)
-	}
-
-	// Get quest ownership list
-	questToOwnersMap := questsToListOfOwnersMap(partyMemberToQuestInventoryMap)
-
-	// Merge all the quest counts together
-	sortedQuestKeys, totalPartyQuests := countTotalUserQuests(allUserQuests)
-
-	// Get quest metadata
-	questMetadata, err := getQuestData()
-	if err != nil {
-		fmt.Println("error getting party members", err.Error())
-		return
-	}
-
-	partyQuestsString := formatPartyQuestData(
-		sortedQuestKeys,
-		totalPartyQuests,
-		questToOwnersMap,
-		questMetadata,
-	)
-
-	partyMemberNames := []string{}
-	for memberName := range partyMemberToQuestInventoryMap {
-		partyMemberNames = append(partyMemberNames, memberName)
-	}
-
-	err = writePartyQuestDataToMarkdown(partyMemberNames, partyQuestsString)
-	if err != nil {
-		fmt.Println("error writing party quest data to md file", err.Error())
-		return
-	}
-
-	for userName, userQuests := range partyMemberToQuestInventoryMap {
-		userQuestData := formatUserQuestData(sortedQuestKeys, userName, userQuests, questMetadata)
-		err = writeUserQuestDataToMarkdown(userName, userQuestData)
+		// Given a partyID, get all the party's members names and UUIDs
+		partyMembers, err := getPartyMembers(myUUID, apiKey, partyID)
 		if err != nil {
-			fmt.Println("error writing user quest data to md file", err.Error())
+			fmt.Println("error getting party members", err.Error())
 			return
 		}
+
+		// Given a list of party members, get all the quests they
+		// have in their inventory
+		allUserQuests := []map[string]int{}
+		partyMemberToQuestInventoryMap := map[string]map[string]int{}
+		for _, member := range partyMembers {
+			userQuests, err := getUserQuests(member.UserUUID)
+			if err != nil {
+				fmt.Println("error getting party members quests", err.Error())
+				return
+			}
+
+			partyMemberToQuestInventoryMap[member.UserName] = userQuests
+			allUserQuests = append(allUserQuests, userQuests)
+		}
+
+		// Get quest ownership list
+		questToOwnersMap := questsToListOfOwnersMap(partyMemberToQuestInventoryMap)
+
+		// Merge all the quest counts together
+		sortedQuestKeys, totalPartyQuests := countTotalUserQuests(allUserQuests)
+
+		// Get quest metadata
+		questMetadata, err := getQuestData()
+		if err != nil {
+			fmt.Println("error getting party members", err.Error())
+			return
+		}
+
+		partyQuestsString := formatPartyQuestData(
+			sortedQuestKeys,
+			totalPartyQuests,
+			questToOwnersMap,
+			questMetadata,
+		)
+
+		partyMemberNames := []string{}
+		for memberName := range partyMemberToQuestInventoryMap {
+			partyMemberNames = append(partyMemberNames, memberName)
+		}
+
+		err = writePartyQuestDataToMarkdown(partyMemberNames, partyQuestsString)
+		if err != nil {
+			fmt.Println("error writing party quest data to md file", err.Error())
+			return
+		}
+
+		for userName, userQuests := range partyMemberToQuestInventoryMap {
+			userQuestData := formatUserQuestData(sortedQuestKeys, userName, userQuests, questMetadata)
+			err = writeUserQuestDataToMarkdown(userName, userQuestData)
+			if err != nil {
+				fmt.Println("error writing user quest data to md file", err.Error())
+				return
+			}
+		}
+
+		time.Sleep(60 * 60 * time.Second)
 	}
 
 	return
